022b30ae56ff806989d46c1d0db7fb41a053fc4c
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2021-02-19 21:12:29 +0100

    Create UserRole.php

---
+++ app/Enums/UserRole.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static Admin()
+ * @method static static ProgramCoordinator()
+ * @method static static Student()
+ */
+final class UserRole extends Enum
+{
+    public const ADMIN = 'admin';
+    public const PROGRAMCOORDINATOR = 'program_coordinator';
+    public const STUDENT = 'student';
+}
